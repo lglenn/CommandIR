b020c9b98b784c474444ff1cb077436b50ab5ec7
Larry Glenn <yrral.glenn@gmail.com>
2011-02-11 20:26:19 -0500

    Now takes an appletv or myth parameter

--- scripts/hdmi_switch.sh
+++ scripts/hdmi_switch.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-command_send.sh 1 monoprice_hdmi_switch $1;
+case "$1" in
+    myth)
+	command="BTN_3";
+	;;
+    appletv)
+	command="BTN_2";
+	;;
+esac
+
+command_send.sh 1 monoprice_hdmi_switch $command;
